17ff57dd798298f4bf119467ed83edcd75e7c1ee
Julian <julian.beutin@viacesi.fr>
2023-08-18 16:29:41 +0200

    :sparkles: à corriger la méthode d'auth dans le back

--- src/main/java/fr/beutin/julian/demo/demo/controller/QuestionController.java
+++ src/main/java/fr/beutin/julian/demo/demo/controller/QuestionController.java
@@ -1,8 +1,11 @@
 package fr.beutin.julian.demo.demo.controller;
 
+import com.nimbusds.jose.proc.SecurityContext;
 import fr.beutin.julian.demo.demo.service.UserService;
 import fr.beutin.julian.demo.demo.dto.MovieDTO;
 import fr.beutin.julian.demo.demo.mapper.MovieMapper;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,15 +15,15 @@ import java.util.List;
 
 @CrossOrigin
 @RestController
-@RequestMapping(path = "/movie")
-public class MovieController {
+@RequestMapping(path = "/question")
+public class QuestionController {
 
 
     private final UserService userService;
 
     private final MovieMapper movieMapper;
 
-    public MovieController(UserService userService, MovieMapper movieMapper) {
+    public QuestionController(UserService userService, MovieMapper movieMapper) {
         this.userService = userService;
         this.movieMapper = movieMapper;
     }
@@ -28,6 +31,7 @@ public class MovieController {
     @GetMapping("")
     public List<MovieDTO> getAllMovies() {
         return movieMapper.map(userService.getMovie());
+
     }
 
 }
--- src/main/java/fr/beutin/julian/demo/demo/security/SecurityConfig.java
+++ src/main/java/fr/beutin/julian/demo/demo/security/SecurityConfig.java
@@ -1,12 +1,5 @@
 package fr.beutin.julian.demo.demo.security;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import fr.beutin.julian.demo.demo.dto.TokenAuth;
-import fr.beutin.julian.demo.demo.service.UserService;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
@@ -19,63 +12,25 @@ import org.springframework.security.web.authentication.session.RegisterSessionAu
 import org.springframework.security.web.authentication.session.SessionAuthenticationStrategy;
 import org.springframework.web.bind.annotation.CrossOrigin;
 
-import java.util.Base64;
 
-
-@CrossOrigin
+@CrossOrigin(origins = "*")
 @Configuration
 @EnableWebSecurity
 class SecurityConfig {
 
-    private final Logger logger = LoggerFactory.getLogger(SecurityConfig.class);
-
-    private final UserService userService;
-
-    SecurityConfig(UserService userService) {
-
-        this.userService = userService;
-    }
 
     @Bean
     protected SessionAuthenticationStrategy sessionAuthenticationStrategy() {
         return new RegisterSessionAuthenticationStrategy(new SessionRegistryImpl());
     }
-
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.csrf(AbstractHttpConfigurer::disable).authorizeHttpRequests(authorize ->
-                authorize.requestMatchers("/*").authenticated());
+                authorize.anyRequest().authenticated());
 
-        httpSecurity.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()).jwt(jwtConfigurer -> {
-           jwtConfigurer.authenticationManager((authentication -> {
-                Object token = authentication.getPrincipal();
-                ObjectMapper objectMapper = new ObjectMapper();
-
-                logger.info(" donnée de l'utilisateur : {}", authentication.getPrincipal());
-
-                Base64.Decoder decoder = Base64.getUrlDecoder();
-                String[] chunks = token.toString().split("\\.");
-
-                logger.info("Données du chunk : {} ", new String(decoder.decode(chunks[1])));
-
-                TokenAuth tokenAuth;
-                try {
-                    tokenAuth = objectMapper.readValue(new String(decoder.decode(chunks[1])), TokenAuth.class);
-                } catch (JsonProcessingException e) {
-                    throw new RuntimeException(e);
-                }
-
-                logger.info(tokenAuth.getEmail());
-
-
-                return authentication;
-            }));
-        }));
-
-
+        httpSecurity.oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()));
 
         return httpSecurity.build();
     }
-
 
 }
