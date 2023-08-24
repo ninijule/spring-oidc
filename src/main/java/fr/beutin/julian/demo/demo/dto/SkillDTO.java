8d37c2119a807920a283acb4a1f4fc08c1f8dddd
Beutin Julian <julian.beutin@outlook.fr>
2023-08-24 02:22:09 +0200

    :sparkles: création du schéma de base propre

--- src/main/java/fr/beutin/julian/demo/demo/dto/SkillDTO.java
+++ src/main/java/fr/beutin/julian/demo/demo/dto/SkillDTO.java
@@ -1,8 +1,13 @@
 package fr.beutin.julian.demo.demo.dto;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class SkillDTO {
 
     private String name;
+
+    private List<TechnologyDTO> technologyDTOS = new ArrayList<>();
 
     public String getName() {
         return name;
@@ -10,5 +15,13 @@ public class SkillDTO {
 
     public void setName(String name) {
         this.name = name;
+    }
+
+    public List<TechnologyDTO> getTechnologyDTOS() {
+        return technologyDTOS;
+    }
+
+    public void setTechnologyDTOS(List<TechnologyDTO> technologyDTOS) {
+        this.technologyDTOS = technologyDTOS;
     }
 }
