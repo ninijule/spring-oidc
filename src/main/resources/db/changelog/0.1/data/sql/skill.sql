4f1f13ec3c485986bf13e036d8d548fa6673d803
Beutin Julian <julian.beutin@outlook.fr>
2023-08-27 12:32:25 +0200

    :sparkles: ajout du schéma liquibase pour les données de tests

---
+++ src/main/resources/db/changelog/0.1/data/sql/skill.sql
@@ -0,0 +1,2 @@
+INSERT INTO skill (name, description) VALUES ('Développement Front-end', 'Permet de faire les aspects visuels du site');
+INSERT INTO skill (name, description) VALUES ('Développement Back-end', 'Permet de gérer les aspects "logiques" du site');
