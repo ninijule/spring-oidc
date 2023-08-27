a8587809b7a893e001025ca4b266ce03c8d40000
Beutin Julian <julian.beutin@outlook.fr>
2023-08-27 12:23:09 +0200

    :sparkles: ajout du schéma liquibase pour les données de tests

---
+++ src/main/resources/db/changelog/0.1/data/sql/job.sql
@@ -0,0 +1,2 @@
+INSERT INTO job (name, description) VALUES ('Développeur Full-Stack', 'Développe des sites webs en maitrisant Front et Back');
+INSERT INTO job (name, description) VALUES ('Tech-Lead', 'Fait genre de travailler');
