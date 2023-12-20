5b150ce7ab1baea383cfef6123f9413c76ad717d
Beutin Julian <julian.beutin@outlook.fr>
2023-12-20 17:17:25 +0100

    :sparkles: rajout des tests

--- src/test/java/fr/beutin/julian/demo/demo/DemoApplicationTests.java
+++ src/test/java/fr/beutin/julian/demo/demo/DemoApplicationTests.java
@@ -1,13 +1,20 @@
 package fr.beutin.julian.demo.demo;
 
+import fr.beutin.julian.demo.demo.controller.GenerateQuestionController;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	private GenerateQuestionController generateQuestionController;
 	@Test
 	void contextLoads() {
+		assertThat(generateQuestionController).isNotNull();
 	}
 
 }
